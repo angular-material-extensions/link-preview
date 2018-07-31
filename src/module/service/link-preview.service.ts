98a2c53bcc813681f1ba2ed5cab764a15e294d4e
Anthony Nahas <anthony.na@hotmail.de>
2018-07-31 21:20:35 +0200

    feat(package): improved the link preview service

--- src/module/service/link-preview.service.ts
+++ src/module/service/link-preview.service.ts
@@ -1,25 +1,35 @@
-import {Injectable} from '@angular/core';
+import {EventEmitter, Injectable, OnDestroy} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {Subscription} from 'rxjs/internal/Subscription';
+import {map} from 'rxjs/operators';
+import {Link} from 'ngx-linkifyjs';
+import {LinkPreview} from '../..';
 
 @Injectable()
-export class LinkPreviewService {
+export class LinkPreviewService implements OnDestroy {
 
   private _accessKey = '5b54e80a65c77848ceaa4630331e8384950e09d392365';
   private _apiURL = 'http://api.linkpreview.net/';
 
+  subscription: Subscription;
+  onLinkFound: EventEmitter<Array<Link>> = new EventEmitter<Array<Link>>();
+
+  links: Link[] = [];
+
   constructor(private http: HttpClient) {
+    this.onLinkFound.subscribe((links: Array<Link>) => this.links = links);
   }
 
-  sayHello(name?: String) {
-    return `Hello ${name || 'Stanger'}!`;
+  ngOnDestroy(): void {
   }
 
-  fetchLink(url: string): Observable<any> {
+  fetchLink(url: string): Observable<LinkPreview> {
+    console.log('fetching the following link: ', url);
     const params = new HttpParams()
       .append('key', this._accessKey)
       .append('q', url);
 
-    return this.http.get(this._apiURL, {params: params});
+    return this.http.get(this._apiURL, {params: params}).pipe(map(value => value as LinkPreview));
   }
 }
