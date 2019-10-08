2ad2c02c52b1c48956edcaafd6e13d5f0bd02d46
Nicholas Seney <nseney@jahnelgroup.com>
2019-10-08 14:48:39 -0400

    fix(MatLinkPreviewDirective.ngOnChanges): removed document input sub and replaced with ngonchanges

--- src/module/directives/mat-link-preview.directive.ts
+++ src/module/directives/mat-link-preview.directive.ts
@@ -1,38 +1,26 @@
-import {Directive, OnInit} from '@angular/core';
-import {fromEvent} from 'rxjs';
-import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
-import {Link, NgxLinkifyjsService} from 'ngx-linkifyjs';
-import {MatLinkPreviewService} from '../../module/service/mat-link-preview.service';
+import { Directive, ElementRef, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Link, NgxLinkifyjsService } from 'ngx-linkifyjs';
+
+import { MatLinkPreviewService } from '../../module/service/mat-link-preview.service';
 
 @Directive({
   selector: '[matLinkPreview]',
   exportAs: '[matLinkPreview]',
 })
-export class MatLinkPreviewDirective implements OnInit {
+export class MatLinkPreviewDirective implements OnChanges {
+  @Input()
+  input: string
 
   constructor(public linkifyService: NgxLinkifyjsService,
               public linkPreviewService: MatLinkPreviewService) {
   }
 
-  ngOnInit(): void {
-    this._init();
-  }
-
-  private _init() {
-    fromEvent(document, 'input')
-      .pipe(
-        debounceTime(2000),
-        distinctUntilChanged(),
-        map(event => {
-          const data = event.target['value'];
-          const links: Link[] = this.linkifyService.find(data);
-          console.log('data: ', data);
-          console.log('links: ', links);
-          // event.target['value'] = this.linkifyService.linkify(data);
-          return links;
-        })).subscribe((links) => {
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.input && changes.input.currentValue) {
+      const data = changes.input.currentValue;
+      const links: Link[] = this.linkifyService.find(data);
       this.linkPreviewService.onLinkFound.emit(links);
-    });
+    }
   }
 
 }
